b813cee95cff4a88a3904c6894e1195ced1a73de
ilja82 <ilja82@users.noreply.github.com>
2026-04-16 19:32:07 +0200

    Bump llm-budget-tracker to v0.0.3 (#3)

--- Casks/llm-budget-tracker.rb
+++ Casks/llm-budget-tracker.rb
@@ -1,6 +1,6 @@
 cask "llm-budget-tracker" do
-  version "0.0.2"
-  sha256 "2f041402104e9ee92f5f98c5fd59a6616c2407094a492be1accef5a37e387001"
+  version "0.0.3"
+  sha256 "120095ab56c68a44055456b1d2a949979a984d64fcd412439b4dd30b8c8875f1"
 
   url "https://github.com/ilja82/llm-budget-tracker/releases/download/v#{version}/LLMBudgetTracker.dmg"
   name "LLM Budget Tracker"
